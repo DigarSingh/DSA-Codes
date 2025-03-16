4337f197948726cef4046e4e27483dfe098a8543
BYTE90 <digarsingh2004@gmail.com>
2025-03-16 17:10:34 +0530

    calloc ()

---
+++ Dynamic_memory_allocation/calloc.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+     //use of calloc
+     int* ptr;
+     int n;
+     printf("Enter the value of array: ");
+     scanf("%d",&n);
+
+     ptr = (int*) calloc( n , sizeof(int));
+
+     for(int i=0;i<n;i++){ //for input
+          printf("Enter the %d element of array: ",i);
+          scanf("%d",&ptr[i]);
+     }
+     
+     for(int i=0;i<n;i++){ //for output
+          printf("%d ",ptr[i]);
+     }
+
+     return 0;
+}
