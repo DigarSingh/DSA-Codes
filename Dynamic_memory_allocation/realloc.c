b798de6eea52e616b81db62c8afad3f4172ad216
BYTE90 <digarsingh2004@gmail.com>
2025-03-16 17:11:18 +0530

    realloc ()

---
+++ Dynamic_memory_allocation/realloc.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+     //use of calloc
+     int* ptr;
+     int n;
+     printf("Enter the value of array: ");
+     scanf("%d",&n);
+
+     ptr = (int*) calloc(n,sizeof(int));
+
+     for(int i=0;i<n;i++){ //for input
+          printf("Enter the %d element of array: ",i);
+          scanf("%d",&ptr[i]);
+     }
+     
+     for(int i=0;i<n;i++){ //for output
+          printf("%d ",ptr[i]);
+     }
+
+     //use of realloc
+     printf("\nEnter the new value of array: ");
+     scanf("%d",&n);
+
+     ptr = (int*) realloc(ptr, n *sizeof(int));
+     
+     for(int i=0;i<n;i++){ //for input
+          printf("Enter the new %d element of array: ",i);
+          scanf("%d",&ptr[i]);
+     }
+     for(int i=0;i<n;i++){ //for output
+          printf("%d ",ptr[i]);
+     }
+     free(ptr);
+     return 0;
+}
